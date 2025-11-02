f58f7198776c5d38c66c2f96ec441540162a5a15
w.alexander <will_alexander@me.com>
2025-11-02 23:55:24 +0100

    Can register contact full slice

--- apps/frontend/src/app/contacts/components/new-contact-form.ts
+++ apps/frontend/src/app/contacts/components/new-contact-form.ts
@@ -5,38 +5,56 @@ import { InputText } from 'primeng/inputtext';
 import { Select } from 'primeng/select';
 import { Button } from 'primeng/button';
 import { ContactsFacade } from '../facades/contacts.facade';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-contact-form',
-  imports: [Card, FloatLabel, InputText, Select, Button],
+  imports: [
+    Card,
+    FloatLabel,
+    InputText,
+    Select,
+    Button,
+    ReactiveFormsModule
+  ],
   template: `
     <p-card header="New Contact">
-      <p-floatlabel variant="in">
-        <input type="text" pInputText id="first-name" />
-        <label for="first-name">First Name</label>
-      </p-floatlabel>
-      <p-floatlabel variant="in">
-        <input type="text" pInputText id="last-name" />
-        <label for="last-name">Last Name</label>
-      </p-floatlabel>
-      <p-floatlabel variant="in">
-        <p-select />
-      </p-floatlabel>
+      <form [formGroup]="newContactForm">
+        <p-floatlabel variant="in">
+          <input type="text" pInputText id="first-name" formControlName="firstName" />
+          <label for="first-name">First Name</label>
+        </p-floatlabel>
+        <p-floatlabel variant="in">
+          <input type="text" pInputText id="last-name" formControlName="lastName" />
+          <label for="last-name">Last Name</label>
+        </p-floatlabel>
+        <p-floatlabel variant="in">
+          <p-select formControlName="source" [editable]="true" id="source"/>
+          <label for="source">Source</label>
+        </p-floatlabel>
+      </form>
       <ng-template #footer>
-        <p-button label="Register" (onClick)="onRegisterContact()"/>
+        <p-button label="Register" (onClick)="onRegisterContact()" />
       </ng-template>
     </p-card>
-  `,
+  `
 })
 export class NewContactForm {
 
   private contactsFacade = inject(ContactsFacade);
+  private formBuilder = inject(FormBuilder);
+
+  newContactForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    source: ['', Validators.required],
+  });
 
   onRegisterContact(): void {
     this.contactsFacade.registerNewContact({
-      firstName: 'Will',
-      lastName: 'Alexander',
-      source: 'LinkedIn',
+      firstName: this.newContactForm.get('firstName')?.value || '',
+      lastName: this.newContactForm.get('lastName')?.value || '',
+      source: this.newContactForm.get('source')?.value || ''
     });
   }
 }
